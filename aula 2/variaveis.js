7012756e0086451be6150dc8ee6cc53a711fa9c0
EmyPopCorn <emillycerqueira099@gmail.com>
2023-03-22 08:28:57 -0300

    Aula02 - Variáveis em JavaScript :)

---
+++ aula 2/variaveis.js
@@ -0,0 +1,23 @@
+let num1 = 44
+const num2 = "com todas as minhas forças"
+var msg = "Odeio Java :)"
+
+
+
+//let usuario = {
+//  nome: "Kuririn",
+//    idade: 42,
+ //   signo: "Indeterminado",
+ //   careca: true
+
+//}
+
+//console.log(typeof usuario)
+//console.log("nome: ", usuario.nome, "\nidade: ", usuario.idade, "\nsigno: ", usuario.signo, "\ncareca: ", usuario.careca)
+//console.log(typeof usuario.careca) 
+
+const nomes = ["Joao", "Siles"]
+
+console.log(nomes)
+console.log(nomes[1])
+console.log(typeof nomes)
